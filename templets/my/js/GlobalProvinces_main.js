f9c41bdf05c46ce204441768afc6a7c4bb17b4b0
zoohang <136927705@qq.com>
2015-10-30 17:48:39 +0800

    999fix

---
+++ templets/my/js/GlobalProvinces_main.js
@@ -0,0 +1,29 @@
+﻿/********** 省份数据 **********/
+var GP = new Array('手机','平板','笔记本','存储介质');
+/********** 市级数据 **********/
+var GC1 = new Array();
+
+GC1['手机']=new Array('Apple（苹果）','Samsung（三星）','Xiaomi（小米）','HTC','lenovo（联想）','MEIZU（魅族）','HUAWEI（华为）','LG','Sony');
+GC1['平板']=new Array('Apple（苹果）','Microsoft','Samsung（三星）','acer','dell','sony','xiaomi');
+GC1['笔记本']=new Array('Apple（苹果）','Samsung（三星）','lenovo（联想）','dell','asus','hp','acer','sony');
+GC1['存储介质']=new Array('西数','希捷','东芝','Samsung（三星）','巫溪','武隆','秀山','永川','酉阳','渝北','渝中','云阳','忠县');
+
+/********** 县乡数据 **********/
+var GC2 = new Array();
+GC2['手机'] = new Array();
+GC2['手机']['Apple（苹果）'] = new Array('iPhone&nbsp;6s&nbsp;plus','iPhone&nbsp;6s','iPhone&nbsp;6p','iPhone&nbsp;6','iPhone&nbsp;5s','iPhone&nbsp;5c','iPhone&nbsp;5','iPhone&nbsp;4s','iPhone&nbsp;4');
+GC2['手机']['Samsung（三星）'] = new Array('阿坝','黑水','红原','金川','九寨沟','理县','马尔康','茂县','壤塘','若尔盖','松潘','汶川','小金','阿坝县');
+GC2['手机']['Xiaomi（小米）'] = new Array('xiaomi&nbsp;note','xiaomi&nbsp;4','xiaomi&nbsp;3','xiaomi&nbsp;2s','xiaomi&nbsp;2','xiaomi&nbsp;1','hongmi&nbsp;note&nbsp;2','hongmi&nbsp;2','hongmi&nbsp;note&nbsp;','hongmi&nbsp;1');
+GC2['手机']['HTC'] = new Array('达县','达州市','大竹','开江','渠县','万源','宣汉','通川区');
+GC2['手机']['lenovo（联想）'] = new Array('德阳市','广汉','罗江','绵竹','什邡','中江','旌阳区');
+GC2['手机']['MEIZU（魅族）'] = new Array('巴塘','白玉','丹巴','道孚','稻城','得荣','德格','甘孜','九龙','康定','理塘','泸定','炉霍','色达','石渠','乡城','新龙','雅江','甘孜县');
+GC2['手机']['HUAWEI（华为）'] = new Array('广安市','华蓥','邻水','武胜','岳池','广安区');
+GC2['手机']['LG'] = new Array('苍溪','广元市','剑阁','青川','旺苍','朝天区','利州区','元坝区');
+GC2['手机']['Sony'] = new Array('峨边','峨眉山','夹江','犍为','井研','乐山市','马边','沐川','金口河区','沙湾区','市中区','五通桥区');
+
+
+
+var GC3 = new Array();
+GC3['手机'] = new Array();
+GC3['手机']['Apple（苹果）'] = new Array();
+GC3['手机']['Apple（苹果）']['iPhone&nbsp;6s&nbsp;plus'] = new Array('进水','不开机','碎屏','无网络','','芜湖路街道','义城镇','淝河镇');
